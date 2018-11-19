c1270bad97b1d2319136bd56f99f0d45b6550be5
riklakhe <rikeshlakhe@gmail.com>
2018-11-19 22:43:37 +0545

    there

---
+++ src/components/contacts/Contacts.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+import Contact from './Contact';
+
+import PropTypes from 'prop-types'
+
+import { connect } from 'react-redux'
+
+import { getContacts } from '../../store/actions/contactActions'
+
+
+class Contacts extends Component {
+  constructor(props){
+    super(props);
+    
+    
+  }
+
+  componentDidMount=()=>{
+    this.props.getContacts();
+  };
+  
+  
+  render() {
+    const { contacts } = this.props;
+    return (
+      <React.Fragment>
+        <h1 className="display-4 mb-2">
+          <span className="text-danger">Contact</span> List
+        </h1>
+        {contacts.map(contact => (
+          <Contact key={contact.id} contact={contact} />
+        ))}
+      </React.Fragment>
+    );
+  }
+}
+
+Contacts.propTypes ={
+  contacts : PropTypes.array.isRequired,
+  getContacts : PropTypes.func.isRequired
+}
+
+
+const mapStateToProps = (state)=>({
+  contacts : state.contact.contacts 
+});
+
+
+// const mapDispatchToProps = (dispatch)=>
+
+
+
+export default connect(mapStateToProps,{getContacts})(Contacts);
